8f5ca8baf8455556c92fd97ed804fc013d9369ef
cliffgallagher <30638496+cliffgallagher@users.noreply.github.com>
2021-07-01 16:26:07 -0500

    Update SoundExchange.java

--- SoundExchange.java
+++ SoundExchange.java
@@ -13,18 +13,19 @@ public class SoundExchange {
         /*
             You should have generated unsorted AM and PM logs for each day, as a result of the first
             two methods.
+            
+            Comment out the two functions above.
 
-            Run:
+            Then, run:
 
-            TimeSorter.createPrintedSortedDirectory
+            TimeSorter.createPrintedSortedDirectory()
 
-            To create the destination file for the sorted logs
+            To create the destination file for the sorted logs. This is done in the commented section below.
 
             Then, create a TimeSorter object for each unsorted AM or PM log, and
-            run sortByTimeAndPrint("unsortedAMorPMlog.txt") on each one
-
-            Below, I used a for loop to do that more quickly
+            run sortByTimeAndPrint("unsortedAMorPMlog.txt") on each one. An example of this is also given below. I used a for loop to do that more quickly.
         */
+        
         /*TimeSorter.createPrintedSortedDirectory();
         
         for (int i = 10; i < 21; i++) {
